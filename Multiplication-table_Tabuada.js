8c4ef6cc99daca333e93ef0e42bd4fab53ac1c67
Fábio Clemente Vilela <fabio_caxa@hotmail.com>
2023-03-01 13:08:10 -0300

    Update and rename Tabuada.js to Multiplication-table_Tabuada.js

--- Multiplication-table_Tabuada.js
+++ Multiplication-table_Tabuada.js
@@ -1,4 +1,4 @@
-//tabuada do 1 ao 10
+//multiplication table from 1 to 10/ Tabuada do 1 ao 10
 for(let i = 1; i <= 10; i++){
     for(let j = 1;j <= 10;j++){
         console.log("|"+ i, "X",j, "=", i*j + "|");
